54d0bf9ea8ee417ceb7919e13f6e27423e2f0058
Bruno Guimarães <brunogdepaula@hotmail.com>
2024-05-28 23:36:27 -0300

    Troca de rotas

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,33 @@
-import { BrowserRouter } from 'react-router-dom'
-
 import Footer from './components/Footer'
 import { GlobalCss, Container } from './styles'
-import Rotas from './routes'
+
 import Cart from './components/Cart'
 import { Provider } from 'react-redux'
 import { store } from './store'
+import { RouterProvider, createBrowserRouter } from 'react-router-dom'
+import Home from './pages/Home'
+import Restaurantes from './pages/Restaurantes'
+
+const rotas = createBrowserRouter([
+  {
+    path: '/',
+    element: <Home />,
+  },
+  {
+    path: '/restaurantes/:id',
+    element: <Restaurantes />,
+  },
+])
 
 function App() {
   return (
     <Provider store={store}>
-      <BrowserRouter>
-        <GlobalCss />
-        <Container>
-          <Rotas />
-          <Footer />
-          <Cart />
-        </Container>
-      </BrowserRouter>
+      <GlobalCss />
+      <Container>
+        <RouterProvider router={rotas} />
+        <Footer />
+        <Cart />
+      </Container>
     </Provider>
   )
 }
